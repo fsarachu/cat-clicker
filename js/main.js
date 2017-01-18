78d147d615a99c80134b7dd679551415281e2f1a
fsarachu <fsarachu@gmail.com>
2017-01-18 19:58:41 -0300

    Cat constructor fixes

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 function Cat(name, image) {
     this.name = name;
-    this.image = "images/" + image;
+    this.image = image;
     this.clicks = 0;
     this.toHtml = function () {
         var html = "<div class='col-sm-6 text-center'>" +
             "<h2>" + this.name + ": <span class='clicks'>" + this.clicks + "</span></h2>" +
-            "<img src='" + this.clicks + "' alt='A picture of " + this.name + "' class='cat img-responsive center-block'>" +
+            "<img src='images/" + this.image + "' alt='A picture of " + this.name + "' class='cat img-responsive center-block'>" +
             "</div>";
 
         return html;
