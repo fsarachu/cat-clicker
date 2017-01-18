45158ee02d104fd515c8000f45c94ffdb483662d
fsarachu <fsarachu@gmail.com>
2017-01-18 19:10:56 -0300

    Implement cat clicker script

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-console.log("it's workin'");
+$(function () {
+    $cat = $("#cat");
+    $clicks = $("#clicks");
+
+    var clickCount = 0;
+    $clicks.text(clickCount);
+
+    $cat.on("click", function () {
+        clickCount += 1;
+        $clicks.text(clickCount);
+    });
+});
