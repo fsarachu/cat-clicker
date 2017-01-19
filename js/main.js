a99718509822d7a02f417360b76ec573ebb8d6bf
fsarachu <fsarachu@gmail.com>
2017-01-19 06:57:04 -0300

    Add id property to Cat and set it inside the forEach loop

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 function Cat(name, image) {
+    this.id = null;
     this.name = name;
     this.image = image;
     this.clicks = 0;
     this.toHtml = function () {
         var html = "<div class='col-sm-6 text-center'>" +
             "<h2>" + this.name + ": <span class='clicks'>" + this.clicks + "</span></h2>" +
-            "<img src='images/" + this.image + "' alt='A picture of " + this.name + "' class='cat img-responsive center-block'>" +
+            "<img id='cat-" + this.id + "' src='images/" + this.image + "' alt='A picture of " + this.name + "' class='cat img-responsive center-block'>" +
             "</div>";
 
         return html;
@@ -20,7 +21,13 @@ $(function () {
 
     var $catContainer = $("#cat-container");
 
-    cats.forEach(function (cat) {
+    cats.forEach(function (cat, index) {
+        cat.id = index;
         $catContainer.prepend(cat.toHtml());
     });
+
+    $("#cat-container").on("click", ".cat", function (target) {
+        null;
+    })
+
 });
