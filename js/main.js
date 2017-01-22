e596af1c7dd2d98e0e8c48a8fe8b0f409382b51c
fsarachu <fsarachu@gmail.com>
2017-01-22 10:10:19 -0300

    Add "visible" property to adminArea view

--- js/main.js
+++ js/main.js
@@ -22,67 +22,67 @@ var model = {
 };
 
 var view = {
-        init: function () {
-            this.displayArea.render();
-            this.catList.render();
-            this.adminArea.hide();
-        },
+    init: function () {
+        this.displayArea.render();
+        this.catList.render();
+        this.adminArea.render();
+    },
+    render: function () {
+        this.displayArea.render();
+        this.catList.render();
+        this.adminArea.render();
+    },
+    displayArea: {
         render: function () {
-            this.displayArea.render();
-            this.catList.render();
-            this.adminArea.render();
-        },
-        displayArea: {
-            render: function () {
-                var cat = octopus.getCurrentCat();
+            var cat = octopus.getCurrentCat();
 
-                var $panel = $("#cat-panel");
-                var $panelHeading = $panel.find(".panel-heading");
-                var $panelBody = $panel.find(".panel-body");
+            var $panel = $("#cat-panel");
+            var $panelHeading = $panel.find(".panel-heading");
+            var $panelBody = $panel.find(".panel-body");
 
-                var $name = $panelHeading.find(".cat-name");
-                $name.text(cat.name);
+            var $name = $panelHeading.find(".cat-name");
+            $name.text(cat.name);
 
-                var $picture = $panelBody.find(".cat-picture");
-                $picture.attr("src", cat.picture);
-                $picture.attr("alt", "A picture of " + cat.name);
-                $picture.attr("data-cat-id", cat.id);
+            var $picture = $panelBody.find(".cat-picture");
+            $picture.attr("src", cat.picture);
+            $picture.attr("alt", "A picture of " + cat.name);
+            $picture.attr("data-cat-id", cat.id);
 
-                var $clicks = $panelBody.find(".cat-clicks");
-                $clicks.text(cat.clicks);
-            }
-        },
-        catList: {
-            render: function () {
-                var cats = octopus.getCats();
-                var $catList = $("#cat-list");
+            var $clicks = $panelBody.find(".cat-clicks");
+            $clicks.text(cat.clicks);
+        }
+    },
+    catList: {
+        render: function () {
+            var cats = octopus.getCats();
+            var $catList = $("#cat-list");
 
-                $catList.html("");
+            $catList.html("");
 
-                cats.forEach(function (cat) {
-                    $catList.append("<a data-cat-id=\"" + cat.id + "\" href=\"#\" class=\"list-group-item\">" + cat.name
-                        + "<span class=\"cat-clicks badge\">" + cat.clicks + "</span></a>");
-                });
-            }
-        },
-        adminArea: {
-            hide: function () {
-                $("#admin-btn").show();
-                $("#admin-panel").hide();
-            },
-            show: function () {
+            cats.forEach(function (cat) {
+                $catList.append("<a data-cat-id=\"" + cat.id + "\" href=\"#\" class=\"list-group-item\">" + cat.name
+                    + "<span class=\"cat-clicks badge\">" + cat.clicks + "</span></a>");
+            });
+        }
+    },
+    adminArea: {
+        visible: false,
+        render: function () {
+            if (this.visible) {
                 $("#admin-btn").hide();
                 $("#admin-panel").show();
-            },
-            render: function () {
+
                 var cat = octopus.getCurrentCat();
                 $("#cat-name-input").val(cat.name);
                 $("#cat-picture-input").val(cat.picture);
                 $("#cat-clicks-input").val(cat.clicks);
+            } else {
+                $("#admin-btn").show();
+                $("#admin-panel").hide();
             }
         }
     }
-    ;
+};
 
 var octopus = {
     init: function () {
@@ -117,7 +117,8 @@ var octopus = {
         var $adminBtn = $("#admin-btn");
 
         $adminBtn.on("click", function () {
-            view.adminArea.show();
+            view.adminArea.visible = true;
+            view.adminArea.render();
         });
 
         var $saveBtn = $("#save-btn");
@@ -137,8 +138,8 @@ var octopus = {
         $cancelBtn.on("click", function ($e) {
             $e.preventDefault();
 
+            view.adminArea.visible = false;
             view.adminArea.render();
-            view.adminArea.hide();
         });
 
         $catList.children().first().trigger("click");
