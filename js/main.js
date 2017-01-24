5924c824066562e62b11c5d8f3fb4a9fb02f3031
fsarachu <fsarachu@gmail.com>
2017-01-24 17:19:48 -0300

    Add level property,  ko.computed()

--- js/main.js
+++ js/main.js
@@ -2,6 +2,23 @@ var ViewModel = function () {
     this.clickCount = ko.observable(0);
     this.name = ko.observable("Bob");
     this.picture = ko.observable("images/bob.jpg");
+    this.level = ko.computed(function () {
+        var level;
+
+        if (this.clickCount() < 3) {
+            level = "Infant";
+        } else if (this.clickCount() < 6) {
+            level = "Toddler";
+        } else if (this.clickCount() < 13) {
+            level = "Child";
+        } else if (this.clickCount() < 21) {
+            level = "Teenager";
+        } else {
+            level = "Adult";
+        }
+
+        return level;
+    }, this);
 
     this.incrementCounter = function () {
         this.clickCount(this.clickCount() + 1);
