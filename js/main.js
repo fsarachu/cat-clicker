b2c441f8ee3b7dbb4992e2f06cfdd95e427d78e7
fsarachu <fsarachu@gmail.com>
2017-01-27 07:03:33 -0300

    Create an observable array of observable cats

--- js/main.js
+++ js/main.js
@@ -21,8 +21,49 @@ var Cat = function (data) {
     }, this);
 };
 
+var initialCats = [
+    {
+        clickCount: 0,
+        name: "Bob",
+        picture: "images/bob.jpg"
+    },
+    {
+        clickCount: 0,
+        name: "Chloe",
+        picture: "images/chloe.jpg"
+    },
+    {
+        clickCount: 0,
+        name: "Milo",
+        picture: "images/milo.jpg"
+    },
+    {
+        clickCount: 0,
+        name: "Samantha",
+        picture: "images/samantha.jpg"
+    },
+    {
+        clickCount: 0,
+        name: "Sophie",
+        picture: "images/sophie.jpg"
+    },
+    {
+        clickCount: 0,
+        name: "Tiger",
+        picture: "images/tiger.jpg"
+    }
+];
+
 var ViewModel = function () {
-    this.currentCat = ko.observable(new Cat({clickCount: 0, name: "Bob", picture: "images/bob.jpg"}));
+    var that = this;
+
+    this.cats = ko.observableArray();
+
+    initialCats.forEach(function (cat) {
+        that.cats.push(ko.observable(new Cat(cat)));
+    });
+
+    this.currentCat = this.cats()[0];
 
     this.incrementCounter = function () {
         this.clickCount(this.clickCount() + 1);
