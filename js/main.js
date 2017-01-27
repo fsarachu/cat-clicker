2c8bc78f605d153d05463cedaa92c99b381f1661
fsarachu <fsarachu@gmail.com>
2017-01-27 06:53:43 -0300

    Now Cat() constructor receives a single object literal with all the data, instead of multiple parameters

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-var Cat = function (name, picture) {
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable(name);
-    this.picture = ko.observable(picture);
+var Cat = function (data) {
+    this.clickCount = ko.observable(data.clickCount);
+    this.name = ko.observable(data.name);
+    this.picture = ko.observable(data.picture);
     this.level = ko.computed(function () {
         var level;
 
@@ -22,7 +22,7 @@ var Cat = function (name, picture) {
 };
 
 var ViewModel = function () {
-    this.currentCat = ko.observable(new Cat("Bob", "images/bob.jpg"));
+    this.currentCat = ko.observable(new Cat({clickCount: 0, name: "Bob", picture: "images/bob.jpg"}));
 
     this.incrementCounter = function () {
         this.clickCount(this.clickCount() + 1);
